b3cdda67ecbbf8f85bff4c03ad04026237fc23f9
vdyakushina <73233065+vdyakushina@users.noreply.github.com>
2021-04-23 14:08:19 +0300

    Add files via upload

---
+++ HW1.py
@@ -0,0 +1,13 @@
+import re
+seq=''
+name=''
+with open('isocitrate lyase.fasta', "r") as inpf:
+  for line in inpf:
+    if ">" not in line:
+      seq+=line.strip()
+    else:
+      name+=line.strip()
+
+namef=re.search(r"\b\w.*\d", name)
+res=re.search(r"K[KR]CGH[LMQR]", seq)
+print(f'“Isocitrate lyase ({namef.group()}) contains its active site {res.group()} starting from {res.start()} position”')
